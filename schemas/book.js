1fc27d1801ddf807c1603acb2a22f84dc0a6ff9b
akholod <andrey.kokhanevich@gmail.com>
2016-11-22 00:58:26 +0200

    add book db schema

---
+++ schemas/book.js
@@ -0,0 +1,17 @@
+/**
+ * Created by andrey on 21.11.16.
+ */
+'use strict';
+var mongoose = require('mongoose');
+
+
+// define the schema for our user model
+var bookSchema = mongoose.Schema({
+    title: { type: String, required: true, unique: true},
+    language: String,
+    image: String,
+    authors: Array,
+    pageCount: Number
+});
+
+module.exports = mongoose.model('Book', bookSchema);
