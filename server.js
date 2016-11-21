7c0784d1866a435efab1c25e8dbc0f8c906b0e05
akholod <andrey.kokhanevich@gmail.com>
2016-11-21 18:30:27 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8000;
+
+//import routes
+let index = require('./routes/index');
+
+//init express app
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', index);
+
+//listen port
+app.listen(port, function() {
+  console.log('App listen at port ' + port);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+module.exports = app;
